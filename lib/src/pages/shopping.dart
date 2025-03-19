3c7c990180293a6010c58272377bd683189abcf6
NathanTorati <nathan.salmaso@sou.unifeob.edu.br>
2025-03-18 21:35:11 -0300

    Update shopping.dart

--- lib/src/pages/shopping.dart
+++ lib/src/pages/shopping.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class RewardsScreen extends StatelessWidget {
@@ -6,125 +5,370 @@ class RewardsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.green,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.emoji_events), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ''),
-        ],
-      ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.green[900],
-            padding: const EdgeInsets.all(20),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 30,
-                  child: Icon(Icons.eco, color: Colors.green[900], size: 40),
+    return Column(
+      children: [
+        Container(
+          width: 393,
+          height: 852,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(color: Color(0xFFE0FEEA)),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                top: 0,
+                child: Container(
+                  width: 393,
+                  height: 217,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF0E4932),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 2,
+                        strokeAlign: BorderSide.strokeAlignOutside,
+                      ),
+                    ),
+                  ),
                 ),
-                const SizedBox(height: 10),
-                const Text(
-                  'Giovana Muniz',
+              ),
+              Positioned(
+                left: 19,
+                top: 780,
+                child: Container(
+                  width: 394,
+                  height: 72,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF0E4932),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 2,
+                        strokeAlign: BorderSide.strokeAlignOutside,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // Ícone 1
+                      Container(
+                        width: 54,
+                        height: 54,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              width: 2,
+                              strokeAlign: BorderSide.strokeAlignOutside,
+                              color: Colors.white,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Image.asset('lib/src/assets/images/home.png',
+                            fit: BoxFit.cover),
+                      ),
+                      // Ícone 2
+                      Container(
+                        width: 54,
+                        height: 54,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(),
+                        ),
+                        child: Image.asset('lib/src/assets/images/book.png',
+                            fit: BoxFit.cover),
+                      ),
+                      // Ícone 3
+                      Container(
+                        width: 54,
+                        height: 54,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Image.asset('lib/src/assets/images/trophy.png',
+                            fit: BoxFit.fitWidth),
+                      ),
+                      // Ícone 4
+                      Container(
+                        width: 54,
+                        height: 54,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Image.asset('lib/src/assets/images/shopping.png',
+                            fit: BoxFit.fitWidth),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 19,
+                top: 59,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: ShapeDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('lib/src/assets/images/plant.png'),
+                      fit: BoxFit.fill,
+                    ),
+                    shape: OvalBorder(
+                      side: BorderSide(
+                        width: 1,
+                        strokeAlign: BorderSide.strokeAlignOutside,
+                      ),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 136,
+                top: 53,
+                child: DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Text('Giovana Muniz'),
+                ),
+              ),
+              Positioned(
+                left: 85,
+                top: 223,
+                child: DefaultTextStyle(
+                  style: TextStyle(
+                    color: Color(0xFF0E4932),
+                    fontSize: 32,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Text('Metas Diárias'),
+                ),
+              ),
+              Positioned(
+                left: 19,
+                top: 300,
+                child: Container(
+                  width: 353,
+                  height: 33,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 319,
+                top: 300,
+                child: Container(
+                  width: 53,
+                  height: 33,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF0E4932),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Image.asset('lib/src/assets/images/add.png',
+                      fit: BoxFit.fitHeight),
+                ),
+              ),
+              Positioned(
+                left: 30,
+                top: 300,
+                child: DefaultTextStyle(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Text('Nova meta'),
+                ),
+              ),
+              Positioned(
+                left: 19,
+                top: 400,
+                child: Container(
+                  width: 353,
+                  height: 33,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 319,
+                top: 400,
+                child: Container(
+                  width: 53,
+                  height: 33,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF0E4932),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Image.asset('lib/src/assets/images/add.png',
+                      fit: BoxFit.fitHeight),
+                ),
+              ),
+              Positioned(
+                left: 30,
+                top: 400,
+                child: DefaultTextStyle(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Text('Nova meta'),
+                ),
+              ),
+              Positioned(
+                left: 19,
+                top: 500,
+                child: Container(
+                  width: 353,
+                  height: 33,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 319,
+                top: 500,
+                child: Container(
+                  width: 53,
+                  height: 33,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF0E4932),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Image.asset('lib/src/assets/images/add.png',
+                      fit: BoxFit.fitHeight),
+                ),
+              ),
+              Positioned(
+                left: 30,
+                top: 500,
+                child: DefaultTextStyle(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Text('Nova meta'),
+                ),
+              ),
+              Positioned(
+                left: 181,
+                top: 101,
+                child: DefaultTextStyle(
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: SizedBox(
+                    width: 150,
+                    child: Text(
+                      'Pontos:  18.750',
+                    ),
                   ),
                 ),
-                const Text(
-                  'Pontos:',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                const Text(
-                  '18.750',
+              ),
+              Positioned(
+                left: 135,
+                top: 138,
+                child: DefaultTextStyle(
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                    height: 1,
+                  ),
+                  child: SizedBox(
+                    width: 242,
+                    height: 21,
+                    child: Text(
+                      '10 dias ajudando a salvar o mundo',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            color: Colors.green[100],
-            width: double.infinity,
-            child: const Text(
-              'Recompensas',
-              style: TextStyle(
-                color: Colors.green,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
               ),
-              textAlign: TextAlign.center,
-            ),
+            ],
           ),
-          Expanded(
-            child: ListView(
-              padding: const EdgeInsets.all(10),
-              children: const [
-                RewardCard(
-                  title: 'Cosméticos',
-                  description: '10% off na compra de produtos de beleza',
-                  points: 20000,
-                ),
-                RewardCard(
-                  title: 'Esportes',
-                  description: 'R\$10,00 off na compra de itens esportivos',
-                  points: 15000,
-                ),
-                RewardCard(
-                  title: 'Moda & Acessórios',
-                  description: '15% off na compra de produtos de beleza',
-                  points: 35000,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RewardCard extends StatelessWidget {
-  final String title;
-  final String description;
-  final int points;
-
-  const RewardCard({
-    super.key,
-    required this.title,
-    required this.description,
-    required this.points,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: ListTile(
-        title: Text(
-          title,
-          style: const TextStyle(fontWeight: FontWeight.bold),
         ),
-        subtitle: Text('$description\nPor: $points pontos'),
-        trailing: IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () {},
-        ),
-      ),
+      ],
     );
   }
 }
